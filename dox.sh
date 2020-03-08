73788e77c4f3d51f07756e20fe00a7d81acd3e53
baldwinvs <baldwinvs@outlook.com>
2020-03-08 15:27:06 -0700

    updated script to check/make doc and generated folders, added doxyfile

--- dox.sh
+++ dox.sh
@@ -3,11 +3,21 @@
 CURR=$PWD
 FILE_PATHS=($CURR/Project)
 
+
 for i in "${FILE_PATHS[@]}"
 do
+    # Check if the doc and doc/generated directories exist
+    if [ -d $i/doc ]; then :
+    else mkdir $i/doc
+    fi
+
+    if [ -d $i/doc/generated ]; then :
+    else mkdir $i/doc/generated
+    fi
+
     cd $i
     echo "Doxygenating in $i"
-    doxygen.exe > nul 2>&1
+    doxygen.exe > /dev/null 2>&1
 done
 
 cd $CURR
